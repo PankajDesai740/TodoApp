43cf56227df58e1a227493ff0b10a73d655a9d49
Pankaj Desai <desaipankaj740@gmail.com>
2022-11-03 20:47:34 +0530

    appbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
-import React from 'react'
+import React from 'react';
+import { AppBar,Toolbar , IconButton , Typography } from '@material-ui/core';
 
 
 
 
 export default function Todos() {
   return (
-    <div>Hello world</div>
+    <div>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton edge="start" color='inherit'>
+                <img src="logo1.png" alt="Todos" style={
+                  {width:"50px"}
+                } />
+          </IconButton>
+          <Typography variant="h5">
+            Todo's 
+          </Typography>
+        </Toolbar>
+      </AppBar>
+    </div>
   )
 }
